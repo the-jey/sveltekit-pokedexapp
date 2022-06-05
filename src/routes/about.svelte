5de4fd9617ef8e4772f4f1d071ccdb7cd7e417ed
JEY <jey.pley@gmail.com>
2022-06-05 15:52:24 +0200

    final commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+	<title>SvelteKit Pokedex | About</title>
+</svelte:head>
+<h1 class="text-4xl text-center my-8 uppercase">About</h1>
+<p>This is a really cool SvelteKit demo that use the Pokemon API to create a Pokedex.</p>
---
+++ src/routes/pokemon/[id].svelte
@@ -0,0 +1,24 @@
+<script context="module">
+	export async function load({ params }) {
+		const id = params.id;
+		const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+		const res = await fetch(url);
+		const pokeman = await res.json();
+		return { props: { pokeman } };
+	}
+</script>
+
+<script>
+	export let pokeman;
+</script>
+
+<div class="p-8 flex flex-col items-center">
+	<h1 class="text-4xl text-center uppercase">{pokeman.name}</h1>
+	<p>
+		Type: <strong class="uppercase">{pokeman.types[0].type.name}</strong> | Height:
+		<strong>{pokeman.height}</strong>
+		| Weight:
+		<strong>{pokeman.weight}</strong>
+	</p>
+	<img class="card-image" src={pokeman.sprites['front_default']} alt={pokeman.name} />
+</div>
